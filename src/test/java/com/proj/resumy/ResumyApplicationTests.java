c4c31af95feedebca29a62fb33e15cbb71fe99b2
seop1284-kr <seop1284@naver.com>
2021-09-15 23:56:19 +0900

    프로젝트 시작

---
+++ src/test/java/com/proj/resumy/ResumyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.resumy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
