7fe363fd0567d07b0d5bdcdbb0560f4403a38538
hahbr88 <hahbr88@hanmail.net>
2021-09-26 15:43:33 +0900

    파일 관리 페이지
    업로드 다운로드 컨트롤러 추가
    FileController.java 삭제

--- src/main/java/com/proj/resumy/file/controller/FileUploadController.java
+++ src/main/java/com/proj/resumy/file/controller/FileUploadController.java
@@ -1,5 +1,15 @@
 package com.proj.resumy.file.controller;
 
-public class FileUploadController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class FileUploadController {
+	// 파일 저장 위치 지정
+	private static final String CURR_FILE_REPO_PATH = "C:\\DevRoot\\Resumy\\Resumy\\files";
+	
+	@RequestMapping(value="/form")
+	public String form() {		//업로드 창인 uploadFrom.jsp를 반환
+		return "uploadForm";
+	}
 }
