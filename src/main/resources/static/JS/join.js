d19f43f98af9624750476ce6b7d80a03e729bbe8
binigy97 <75118895+binigy97@users.noreply.github.com>
2021-10-08 16:52:52 +0900

    로그인, 회원가입 view
    **회원가입에 ID/PW 찾기, 소셜로그인 안 쓸 경우 주석 필요
    
    **회원가입 js 로직 추가해야 됨 : id 중복확인, 이메일 인증번호 확인
    
    **회원가입 js 디버깅 해야 됨 : select 태그를 이용하는 생년월일 js 중에서 어떤 월을 선택해도 31일만 나옴

---
+++ src/main/resources/static/JS/join.js
@@ -0,0 +1,177 @@
+$(function(){
+	
+	// id alert
+	$('#chkIdErrorExist').hide(); // 존재 아이디 오류
+	$('#chkIdErrorNull').hide();  // 아이디 입력없이 중복확인 누른 오류
+	$('#chkIdSuccess').hide();    // 아이디 중복확인 성공
+	
+	// pw alert
+	$('#chkPwErrorRE').hide();	  // 비밀번호 양식 오류
+	$('#chkPwErrorNull').hide();  // 비밀번호 입력없이 재확인 입력한 오류
+	$('#chkPwSuccess').hide();    // 비밀번호 확인 성공
+	
+	// email alert
+	$('#chkEmailSuccessForm').hide();    // 이메일 인증번호 입력칸
+	$('#chkEmailErrorNull').hide();	     // 이메일 주소 채우지 않고 확인버튼 누른 오류
+	$('#chkEmailErrorInputNull').hide(); // 이메일 인증 미완료(입력X)
+	$('#chkEmailFail').hide();           // 이메일 인증 실패
+	$('#chkEmailSuccess').hide();        // 이메일 인증 성공
+	
+	checkPassword(); // 비밀번호 확인
+	
+	// select option 년도 채우기
+	insertYear();
+	
+	// select option 월 채우기
+	insertMonth();
+	
+	// select option 일 채우기
+	insertDate();
+	
+});
+
+// 아이디 중복확인
+function checkId() {
+	var id = $('#userid').val();
+	switch(id) {
+		case "": // 아이디를 입력하지 않고 중복확인 버튼 누름
+			$('#chkIdErrorExist').hide();
+			$('#chkIdErrorNull').show();
+			$('#chkIdSuccess').hide();
+			break;
+		case "same": // 동일한 아이디 존재 <<<<<<<<<<<<<<< 동일한 아이디 확인에 대한 로직 구현 필요
+			$('#chkIdErrorExist').show();
+			$('#chkIdErrorNull').hide();
+			$('#chkIdSuccess').hide();
+			break;
+		default: // 동일한 아이디 존재X
+			$('#chkIdErrorExist').hide();
+			$('#chkIdErrorNull').hide();
+			$('#chkIdSuccess').show();
+	}
+}
+
+
+// 비밀번호 확인
+function checkPassword() {
+	$('#chkPassword').keyup(function() {
+		$('#password').blur();
+		
+		var password = $('#password').val();
+		var chkPassword = $('#chkPassword').val();
+		
+		if(password == "" && chkPassword != "") { // 비밀번호 입력 안 하고 비밀번호 재입력
+			$('#chkPwErrorRE').hide();
+			$('#chkPwErrorNull').show();
+			$('#chkPwSuccess').hide();
+			$('#password').focus();
+		} else if(password != chkPassword) { // 비밀번호와 비밀번호 재입력이 안 맞음
+			$('#chkPwErrorRE').show();
+			$('#chkPwErrorNull').hide();
+			$('#chkPwSuccess').hide();
+		} else if(password == chkPassword && password != "") { // 비밀번호와 비밀번호 재입력이 맞음
+			$('#chkPwErrorRE').hide();
+			$('#chkPwErrorNull').hide();
+			$('#chkPwSuccess').show();
+		}
+	});
+	
+	$('#password').keyup(function() {
+		var password = $('#password').val();
+		var chkPassword = $('#chkPassword').val();
+		
+		if(password != "" && chkPassword == "") {
+			$('#chkPwErrorRE').hide();
+			$('#chkPwErrorNull').hide();
+			$('#chkPwSuccess').hide();
+		}
+	});
+}
+
+// select option 년도 채우기
+function insertYear() {
+	var today = new Date();   
+	var year = today.getFullYear(); // 년도
+	
+	year = year - 12; // 만 12세 이상부터
+	for(i = 0; year > 1900; i++) {
+		year = year - 1;
+		$('#year').append('<option value="' + year + '">' + year + '</option>');
+	}
+}
+
+// select option 월 채우기
+function insertMonth() {
+	for(i = 1; i <= 12; i++) {
+		$('#month').append('<option value="' + i + '">' + i + '</option>');
+	}
+}
+
+// select option 일 채우기
+function insertDate() {
+	$('#date').click(function() {
+		var month = $('#month').val();
+		if(month == -1) {
+			alert('태어난 월을 먼저 선택해주세요.');
+		} else {
+			for(i = 1; i <= 31; i++) { // <<<<<<<<<<< 뭘 해도 31만 나옴...
+				switch(month) {
+					case 2:
+						if (i == 30) break;
+						$('#date').append('<option value="' + i + '">' + i + '</option>');
+						break;
+					case 4:
+					case 6:
+					case 9:
+					case 11:
+						if (i == 31) break;
+						$('#date').append('<option value="' + i + '">' + i + '</option>');
+						break;
+					default:
+						$('#date').append('<option value="' + i + '">' + i + '</option>');
+						break;
+				}
+			}
+		}
+	});
+}
+
+// 이메일 인증 <<<<<<<<< ????? 아무것도 안 보여야하는데 왜 성공 alert가 보이지?
+function checkEmail() {
+	var prefixEmail = $('#prefixEmail').val();
+	var suffixEmail = $('#suffixEmail option:selected').val();
+	
+	$('#chkEmailErrorInputNull').hide();
+	$('#chkEmailFail').hide();
+	$('#chkEmailSuccess').hide();
+		
+	if(prefixEmail == "" || suffixEmail == "") { // 이메일을 입력하지 않고 인증하기 버튼 누름
+		$('#chkEmailErrorNull').show();
+	}
+	if(prefixEmail != "" && suffixEmail != -1) { // 정상수행 후 인증하기 버튼 누름
+		$('#chkEmailErrorNull').hide();
+		$('#chkEmailSuccessForm').show(); // 인증번호 입력폼 등장
+	}
+}
+
+// 이메일 인증확인
+function checkEmailSuccess() {
+	var chkEmailSuccessInput = $('#chkEmailSuccessInput').val();
+	var sendEmailText = 'same'; // <<<<<<<<<<<<<<<<<<<<<<<<< 이메일 인증번호 가져오는 로직 추가해야 함
+	
+	$('#chkEmailErrorNull').hide();
+	
+	if(chkEmailSuccessInput == "") { // 인증번호 기입하지 않고 확인 버튼 누름
+		$('#chkEmailErrorInputNull').show();
+		$('#chkEmailFail').hide();
+		$('#chkEmailSuccess').hide();
+	} else if(chkEmailSuccessInput != sendEmailText) { // 인증번호가 다름
+		$('#chkEmailErrorInputNull').hide();
+		$('#chkEmailFail').show();
+		$('#chkEmailSuccess').hide();
+	} else if(chkEmailSuccessInput == sendEmailText) { // 인증성공
+		$('#chkEmailErrorInputNull').hide();
+		$('#chkEmailFail').hide();
+		$('#chkEmailSuccess').show();
+	}
+}
--- src/main/webapp/WEB-INF/views/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/loginForm.jsp
@@ -3,19 +3,80 @@
 <!DOCTYPE html>
 <html lang="ko">
 <head>
-<meta charset="UTF-8">
-<title>로그인 페이지</title>
-</head>
-<body>
-<h1>로그인 페이지</h1>
-<form action="/loginOk" method="POST"> <%-- loginProcessingUrl() 로 세팅한 url, 반드시 POST! --%>
-	<%-- 일단 아이디/패스워드 의 name 은 'username' 과 'password' 로 하자 (시큐리티의 디폴트) --%>
-	<%-- 만약 다른 name  을 사용하려면 SecurityConfig 에서 .usernameParameter("userid") 해주면 된다 --%>
-	<input type="text" name="userid" placeholder="아이디 입력"/><br>
-	<input type="password" name="password" placeholder="패스워드 입력"/><br>
-	<button>로그인</button>
-</form>
-<a href="/join">회원가입을 아직 하지 않으셨나요</a>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Resumy - 로그인</title>
+	<!-- 파비콘 -->
+	<link href="/img/logo_sm.png" rel="shortcut icon" type="image/x-icon">
+	<!-- FontAwesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+	<!-- Bootstrap CSS -->
+	<link href="/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+	<style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
 
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+	<!-- My CSS -->
+	<link href="/css/login.css" rel="stylesheet">
+</head>
+<%-- loginProcessingUrl() 로 세팅한 url, 반드시 POST! --%>
+<%-- 일단 아이디/패스워드 의 name 은 'username' 과 'password' 로 하자 (시큐리티의 디폴트) --%>
+<%-- 만약 다른 name 을 사용하려면 SecurityConfig 에서 .usernameParameter("userid") 해주면 된다 --%>
+<body class="text-center">
+
+    <form class="form-login" action="/loginOk" method="POST">
+    
+      <!-- logo -->
+	  <a href="/"><img class="mb-4" src="/img/logo_shadow.png" alt="Resumy 로고" width="300"></a>
+	  
+	  <!-- id/pw -->
+	  <label for="inputID" class="sr-only">Id</label>
+	  <input type="text" id="inputID" class="form-control" placeholder="아이디" required autofocus>
+	  <label for="inputPassword" class="sr-only">Password</label>
+	  <input type="password" id="inputPassword" class="form-control" placeholder="비밀번호" required>
+	  <div class="checkbox mb-2 d-flex justify-content-between">
+	    <label>
+	      <input type="checkbox" value="remember-me"> 로그인 상태 유지
+	    </label>
+	    <label>
+	    	<span><a href="#">ID/PW 찾기</a></span>
+	    </label>
+	  </div>
+	  
+	  <!-- login button -->
+	  <button class="btn btn-lg btn-primary btn-block border-0 mb-4" type="submit">로그인</button>
+	  <button class="btn btn-lg btn-primary btn-block border-0 btn-kk d-flex justify-content-center align-item-center" type="button">
+	  	<i class="fas fa-comments"></i>
+	  	<span>카카오톡으로 로그인</span>
+	  </button>
+	  <button class="btn btn-lg btn-primary btn-block border-0 btn-fb d-flex justify-content-center align-item-center" type="button">
+	  	<i class="fab fa-facebook-square"></i>
+	  	<span>Facebook으로 로그인</span>
+	  </button>
+	  <button class="btn btn-lg btn-primary btn-block border-0 btn-nv d-flex justify-content-center align-item-center" type="button">
+	  	<i class="fas">N</i>
+	  	<span>네이버로 로그인</span>
+	  </button>
+	  
+	  <!-- signin -->
+	  <div class="mt-3" style="font-size: 14px">
+	  	  아직 회원이 아니시면 눌러주세요
+		  <a href="/join">회원가입</a>
+	  </div>
+	  
+	</form>
+	
 </body>
 </html>
