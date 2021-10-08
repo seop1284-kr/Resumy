b5db7f0ffad3ee2fd341680bff4704f4ecd82548
hahbr88 <hahbr88@hanmail.net>
2021-10-08 19:26:25 +0900

    메인페이지
    자바스크립트 채용속보 api

--- src/main/resources/static/JS/index.js
+++ src/main/resources/static/JS/index.js
@@ -1,17 +1,19 @@
 /*
 공채 속보 API
 
-기업로고 <regLogImgNm>
-채용업체명 <empBusiNm>
-채용 분야 <empWantedTypeNm>
+★기업로고 <regLogImgNm>
+★채용제목 <empWantedTitle>
+★채용업체명 <empBusiNm>
+★기업구분 <coClcdNm>
+★고용형태 <empWantedTypeNm>
 지원자격(경력) <empWantedCareerNm>
 지원자격(학력) <empWantedEduNm>
 근무지 <workRegionNm>
 채용시작일 <empWantedStdt>
-채용마감일 <empWantedEndt>
+★채용마감일 <empWantedEndt> <- 이걸로 공고 종료일 d-day 구하기
 
-채용기업 홈페이지 <empWantedHomepg>
-채용사이트URL <empWantedHomepgDetail>
+★채용기업 홈페이지 <empWantedHomepg>
+★채용사이트URL <empWantedHomepgDetail> - 현재 로고에 적용되어 있는 url
 */
 
 $(document).ready(function() {
@@ -35,9 +37,12 @@ function parseJson(data) {
 	
 	for (var i = 0; i < info.length; i++) {
 		table += "<tr>\n";
+		table += "<td><a href='" + info[i].empWantedHomepgDetail + "'><img src ='" + info[i].regLogImgNm + "' alt='" + info[i].empWantedTitle + "'></a></td>\n";
 		table += "<td>" + info[i].empWantedTitle + "</td>\n";
 		table += "<td>" + info[i].empBusiNm + "</td>\n";
 		table += "<td>" + info[i].coClcdNm + "</td>\n";
+		table += "<td>" + info[i].empWantedTypeNm + "</td>\n";
+		table += "<td>" + info[i].empWantedEndt + "</td>\n";
 		table += "</tr>\n";
 	}
 	
