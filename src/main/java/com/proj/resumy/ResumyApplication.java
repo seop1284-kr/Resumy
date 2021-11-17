48a242c79d02ff528b982fea848f4329c9ce97d7
seop1284-kr <seop1284@naver.com>
2021-11-17 16:53:33 +0900

    war 배포 위한 pom.xml, 프로젝트 메인 클래스 수정

--- src/main/java/com/proj/resumy/ResumyApplication.java
+++ src/main/java/com/proj/resumy/ResumyApplication.java
@@ -2,12 +2,19 @@ package com.proj.resumy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ResumyApplication {
+public class ResumyApplication extends SpringBootServletInitializer {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResumyApplication.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ResumyApplication.class);
+	}
 }
